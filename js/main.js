c3799548270a5c59d9f3e47ed33c8972992f76e9
Jarnehall <alexander@jarnehall.se>
2015-11-19 20:24:36 +0100

    completed basic navigation functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var menuBtn = document.getElementById('menuIcon'),
    nav = document.getElementById('nav');
    head = document.getElementById('head');
-   activated = 0;
+   scrollActive = 0;
 
 menuBtn.addEventListener('click', function (){
    toggleClass(menuBtn, 'active');
@@ -10,17 +10,18 @@ menuBtn.addEventListener('click', function (){
 });
 
 window.onscroll = function() {
-  console.log(activated);
-  if (window.scrollY > 10 && activated == 0) {
-    toggleClass(head, 'scrolled');
-    toggleClass(nav, 'scrolled');
-    activated = 1;
-  } else if (window.scrollY < 10 && activated == 1) {
-    toggleClass(head, 'scrolled');
-    toggleClass(nav, 'scrolled');
-    activated = 0;
-  }
+  scrolling(head, 'fixed', '50');
 };
+
+function scrolling(obj, customClass, y) {
+  if (window.scrollY > y && scrollActive === 0) {
+    toggleClass(obj, customClass);
+    scrollActive = 1;
+  } else if (window.scrollY < y && scrollActive === 1) {
+    toggleClass(obj, customClass);
+    scrollActive = 0;
+  }
+}
 
 function toggleClass(el, ourClass){
    var classes = el.className;
